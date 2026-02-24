d977bcd341642a70ef6a86eb2e8b0121560cb4f9
风雪心 <1097071510@qq.com>
2026-02-24 21:30:42 +0800

    feat: 增加了手动实现connect模块

--- React系列/test/src/App.jsx
+++ React系列/test/src/App.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
-import Index from './components/Index.jsx';
-import HOC from './hoc/Hoc.jsx';
+import store from './redux/index.js'
+import {Provider} from './context/ReduxContext.jsx';
+import Child from './components/Child.jsx';
 
-const NewIndex=HOC(Index);
+
 export default function App() {
   return (
-    <div>
-        <NewIndex />
-    </div>
+    <Provider store={store}>
+      {/* 注意：由于在context中定义provider时，参数设定为store和children，所以这里不同于默认的value，为store */}
+        <Child />
+    </Provider>
   )
 }
-
-/**
- *    本例在HOC中封装了对象的初始值，以及操作原组件属性的方法，通过父子组件通信的方式将其传递给子组件。
- *    子组件内部通过props获取父组件传递过来的方法，并在组件内部进行使用。
- */
